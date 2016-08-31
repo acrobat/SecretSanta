630dbcf239bcfc5704daf0b91b6cc48980a6d368
Joeri Timmermans <joeri.timmermans@intracto.com>
2016-08-31 17:42:00 +0200

    Upgrade composer + introduction domain

---
+++ src/Domain/Entry/EntryRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Domain\Entry;
+
+use Doctrine\ORM\EntityRepository;
+
+class EntryRepository extends EntityRepository
+{
+    /**
+     * @return Entry[]
+     */
+    public function findAfter(\DateTime $startDate)
+    {
+        $query = $this->_em->createQuery('
+            SELECT entry
+            FROM IntractoSecretSantaBundle:Entry entry
+            JOIN entry.pool pool
+            JOIN entry.entry peer
+            WHERE pool.sentdate >= :startDate
+              AND peer.wishlist IS NOT NULL
+        ');
+        $query->setParameter('startDate', $startDate, \Doctrine\DBAL\Types\Type::DATETIME);
+
+        return $query->getResult();
+    }
+}
