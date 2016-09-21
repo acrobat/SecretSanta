82746e6d91fe8137b558bbe47b805db0e4e38a2c
Jeroen Thora <jeroenthora@gmail.com>
2016-09-21 09:15:26 +0200

    travis config + behat

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+phpenv config-rm xdebug.ini || exit $? # Disable XDebug
 mkdir -p \"${BUILD_CACHE_DIR}\" || exit $? # Create build cache directory
 
 # Update composer to the latest stable release as the build env version is outdated
---
+++ src/Intracto/SecretSantaBundle/Features/setup_test.feature
@@ -0,0 +1,4 @@
+Feature: Check if travis is setup correctly
+    Scenario:
+        Given I am on "/"
+        Then I should see "What is Secret Santa?"
